06a97814c49830b23c2e4b514189017c7f0eff21
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2024-09-26 11:18:46 -0300

    feat: add support to flatpak packages

--- scripts/install-system-packages.sh
+++ scripts/install-system-packages.sh
@@ -9,4 +9,9 @@ echo "Installing native packages..."
 install_nala
 sudo nala upgrade -y
 sudo nala install -y $PACKAGES
-echo "native packages installed."
+echo "Native packages installed."
+
+echo "Enabling Flatpak..."
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+echo "Flatpak enabled."
