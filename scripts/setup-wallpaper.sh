9cd08f434b449301fa0acd24ebce1de6400a003a
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-08-10 14:36:56 -0300

    feat: call setup wallpaper

--- scripts/setup-wallpaper.sh
+++ scripts/setup-wallpaper.sh
@@ -1,14 +1,10 @@
 #! /bin/bash
 
-PWD=$(pwd)
+WALLPAPER_DIR=/usr/share/wallpapers/SpaceFun/gnome-background.xml
 
 echo "Setting up Wallpaper..."
 
-echo "Copying wallpaper..."
-sudo mkdir -p /usr/share/backgrounds/user
-sudo cp "${PWD}/assets/wallpaper.png" /usr/share/backgrounds/user/default.png
-
 echo "Setting wallpaper..."
-gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/user/default.png"
-gsettings set org.gnome.desktop.background picture-uri-dark "file:///usr/share/backgrounds/user/default.png"
-gsettings set org.gnome.desktop.screensaver picture-uri "file:///usr/share/backgrounds/user/default.png"
+gsettings set org.gnome.desktop.background picture-uri "file://${WALLPAPER_DIR}"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${WALLPAPER_DIR}"
+gsettings set org.gnome.desktop.screensaver picture-uri "file://${WALLPAPER_DIR}"
