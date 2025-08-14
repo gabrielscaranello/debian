113d4190698ec6543e0677b1dfc380edbf2ce98d
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-08-14 19:28:00 -0300

    chore: use repo docker version

--- scripts/add-missing-repos.sh
+++ scripts/add-missing-repos.sh
@@ -8,20 +8,6 @@ _add_contrib_nonfree_repo() {
   echo "Contrib and non-free repository added."
 }
 
-_add_docker_repo() {
-  echo "Adding Docker repo..."
-  echo "Removing old files if exists..."
-  sudo rm -rf /etc/apt/keyrings/docker.gpg /etc/apt/sources.list.d/docker.list
-
-  echo "Adding Docker repo..."
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
-  echo "Docker repo added."
-}
-
 _add_firefox_repo() {
   echo "Adding Firefox repo..."
   echo "Removing old files if exists..."
@@ -69,7 +55,6 @@ EOF
 
 echo "Adding missing deb repos..."
 _add_contrib_nonfree_repo
-_add_docker_repo
 _add_firefox_repo
 _add_papirus_repo
 _add_vscode_repo
