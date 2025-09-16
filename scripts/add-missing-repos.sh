dfcc36e39f6d7fbbcad4e2cc39d8db31e7b79ecc
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2025-09-16 15:45:27 -0300

    chore: update add repo script

--- scripts/add-missing-repos.sh
+++ scripts/add-missing-repos.sh
@@ -14,22 +14,16 @@ _add_firefox_repo() {
   sudo rm -rf /etc/apt/keyrings/packages.mozilla.org.asc /etc/apt/sources.list.d/mozilla.list
 
   echo "Adding Firefox repo..."
-  wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
-  echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list >/dev/null
+  wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+  cat <<EOF | sudo tee /etc/apt/sources.list.d/mozilla.sources >/dev/null
+Types: deb
+URIs: https://packages.mozilla.org/apt
+Suites: mozilla
+Components: main
+Signed-By: /etc/apt/keyrings/packages.mozilla.org.asc
+EOF
 
   echo "Firefox repo added."
-}
-
-_add_papirus_repo() {
-  echo "Adding Papirus repo..."
-  echo "Removing old files if exists..."
-  sudo rm -rf /etc/apt/sources.list.d/papirus-ppa.list /etc/apt/trusted.gpg.d/papirus-ppa.asc
-
-  echo "Adding Papirus repo..."
-  sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu noble main' > /etc/apt/sources.list.d/papirus-ppa.list"
-  sudo wget -qO /etc/apt/trusted.gpg.d/papirus-ppa.asc 'https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9461999446FAF0DF770BFC9AE58A9D36647CAE7F'
-
-  echo "Papirus repo added."
 }
 
 _add_vscode_repo() {
@@ -56,7 +50,6 @@ EOF
 echo "Adding missing deb repos..."
 _add_contrib_nonfree_repo
 _add_firefox_repo
-_add_papirus_repo
 _add_vscode_repo
 
 sudo apt update
